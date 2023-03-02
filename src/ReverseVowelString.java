574e301c1c0f96ac80833fcfb25796f328c6c163
DEVPATEL2611 <devvpatel2611@gmail.com>
2023-03-02 23:21:25 +0530

    commit on 02-03-2023

---
+++ src/ReverseVowelString.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ReverseVowelString {
+
+    public  static  void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+        String s = sc.next();
+        String ans = reverseVowel(s);
+
+        System.out.println(ans);
+    }
+
+    public static String reverseVowel(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        char[] ch = s.toCharArray();
+        while (i < j) {
+            if ((ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' || ch[i] == 'A'
+                    || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U')
+                    &&
+                    (ch[j] == 'a' || ch[j] == 'e' || ch[j] == 'i' || ch[j] == 'o' || ch[j] == 'u' || ch[j] == 'A'
+                            || ch[j] == 'E' || ch[j] == 'I' || ch[j] == 'O' || ch[j] == 'U')) {
+                char temp = ch[i];
+                ch[i] = ch[j];
+                ch[j] = temp;
+                i++;
+                j--;
+            } else if (!(ch[i] == 'a' || ch[i] == 'e' || ch[i] == 'i' || ch[i] == 'o' || ch[i] == 'u' ||
+                    ch[i] == 'A' || ch[i] == 'E' || ch[i] == 'I' || ch[i] == 'O' || ch[i] == 'U')) {
+                i++;
+            } else if (!(ch[j] == 'a' || ch[j] == 'e' || ch[j] == 'i' || ch[j] == 'o' || ch[j] == 'u' ||
+                    ch[j] == 'A' || ch[j] == 'E' || ch[j] == 'I' || ch[j] == 'O' || ch[j] == 'U')) {
+                j--;
+            }
+        }
+        return new String(ch);
+    }
+}
